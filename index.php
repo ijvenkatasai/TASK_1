2b77caa1e674339be2d4a5740b42da801a1de5b4
ijvenkatasai <ijvenkatasai@gmail.com>
2025-09-13 22:26:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -33,11 +33,12 @@
         <input type="submit" name="signin" value="Submit">
     </form>
     <?php
-$username=$_POST["username"];
-$password=$_POST["password"];
-echo "USERNAME:{$username} <br>";
-echo "PASSWORD:{$password}";
-
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $username = $_POST["username"] ?? '';
+    $password = $_POST["password"] ?? '';
+    echo "USERNAME: {$username} <br>";
+    echo "PASSWORD: {$password}";
+}
 ?> 
     </div>
     
